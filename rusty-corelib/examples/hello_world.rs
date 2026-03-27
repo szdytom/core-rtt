f93b6121db212bc43dd4adefca8f4d9bdbda2463
szdytom <szdytom@qq.com>
2026-03-28 01:17:36 +0800

    [rusty-corelib] add initial implementation of rusty-corelib
    Signed-off-by: szdytom <szdytom@qq.com>

---
+++ rusty-corelib/examples/hello_world.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+
+use core::hint::spin_loop;
+
+use rusty_corelib::{entry, logln};
+
+fn run() -> ! {
+    let dev_info = rusty_corelib::dev_info();
+    let my_runtime = if dev_info.id == 0 { "Base" } else { "Unit" };
+    let _ = logln!("Hello world from {} runtime!", my_runtime);
+
+    let info = rusty_corelib::meta();
+    if let Ok(game) = info {
+        let _ = logln!(
+            "[base] map={}x{} base_size={}",
+            game.map_width,
+            game.map_height,
+            game.base_size
+        );
+    }
+
+    loop {
+        spin_loop();
+    }
+}
+
+entry!(run);
---
+++ rusty-corelib/src/error.rs
@@ -0,0 +1,50 @@
+#[repr(i32)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum ErrorCode {
+    InvalidPointer = -1,
+    InvalidUnit = -2,
+    InsufficientEnergy = -3,
+    OnCooldown = -4,
+    CapacityFull = -5,
+    InvalidId = -6,
+    OutOfRange = -7,
+    UnsupportedRuntime = -8,
+    Unknown = i32::MIN,
+}
+
+pub type Result<T> = core::result::Result<T, ErrorCode>;
+
+impl ErrorCode {
+    #[must_use]
+    pub const fn from_raw(code: i32) -> Self {
+        match code {
+            -1 => Self::InvalidPointer,
+            -2 => Self::InvalidUnit,
+            -3 => Self::InsufficientEnergy,
+            -4 => Self::OnCooldown,
+            -5 => Self::CapacityFull,
+            -6 => Self::InvalidId,
+            -7 => Self::OutOfRange,
+            -8 => Self::UnsupportedRuntime,
+            _ => Self::Unknown,
+        }
+    }
+}
+
+#[must_use]
+pub const fn status(code: i32) -> Result<()> {
+    if code == 0 {
+        Ok(())
+    } else {
+        Err(ErrorCode::from_raw(code))
+    }
+}
+
+#[must_use]
+pub const fn value(code: i32) -> Result<usize> {
+    if code >= 0 {
+        Ok(code as usize)
+    } else {
+        Err(ErrorCode::from_raw(code))
+    }
+}

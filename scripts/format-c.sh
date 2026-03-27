96806ef0b9841f061e40c77570758ec5759cfc89
szdytom <szdytom@qq.com>
2026-03-27 17:54:18 +0800

    [scripts] make format-c.sh check clang-format version
    Signed-off-by: szdytom <szdytom@qq.com>

--- scripts/format-c.sh
+++ scripts/format-c.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 cd "$(git rev-parse --show-toplevel)"
+
+# Check if clang-format is of version 22 or higher
+if ! clang-format --version | grep -q "version 2[2-9]"; then
+	echo "Error: clang-format version 22 or higher is required."
+	echo "Please install clang-format 22 or higher and try again."
+	exit 1
+fi
+
 find . -type d -name build -prune -o -type f \( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -print0 | xargs -0 clang-format -i
